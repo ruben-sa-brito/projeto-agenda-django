d0365630d9931b784e9acac8bbf4762542d1b8aa
Timewound <rubenjms1@gmail.com>
2023-07-11 10:49:15 -0300

    adicionando contact ao django admin

--- contact/models.py
+++ contact/models.py
@@ -8,3 +8,7 @@ class Contact(models.Model):
     email = models.EmailField( max_length = 254, blank = True )
     creat_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank = True)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
