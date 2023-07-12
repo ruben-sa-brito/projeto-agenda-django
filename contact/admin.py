8710f1fab2c1c3273bb3e8fd563d87b87992f4c7
Timewound <rubenjms1@gmail.com>
2023-07-12 10:59:18 -0300

    adicionando media root

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id','first_name','last_name', 'phone',)
+    ordering = ('id',)
+    search_fields = ('id', 'first_name',)
+    list_display_links = ('id', 'first_name',)
+    list_per_page = 1
+    list_max_show_all = 100
