37d04b84108300e94e8fc9abd03caaa9bdc5f53b
Timewound <rubenjms1@gmail.com>
2023-07-28 10:13:12 -0300

    adicionando model categoria e adicionando no admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from contact import models
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('id',)
+    
+    
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id','first_name','last_name', 'phone',)
