8f484800ffc757e7a5d2698521fc36b254c6f521
Timewound <rubenjms1@gmail.com>
2023-08-25 10:30:03 -0300

    adicionando partials e incluindo mecanismo de busca

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from contact.views import index, contact
+from contact.views import index, contact, search
 
 app_name = 'contact'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('search/', search, name='search'),
     path('<int:contact_id>/', contact, name='contact'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -10,6 +11,7 @@ def index(request):
     
     context = {
         'contacts':contacts,
+        'site_title':'Contatos - '
     }
     
     return render(
@@ -22,16 +24,45 @@ def contact(request, contact_id):
     
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
+    
     if single_contact is None:
         raise Http404()
     
     context = {
         'contact':single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} -'
     }
     
     return render(
         request,
         'contact/contact.html', 
         context
-    )    
+    )   
+
+def search(request):
+    print(request.GET)
+    
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return(redirect('contact:index'))
+    
+    contacts = Contact.objects\
+    .filter(show=True)\
+    .filter(Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value))\
+    .order_by('-id')
+    
+    context = {
+        'contacts':contacts,
+        'site_title':'Search - '
+    }
+    
+    return render(
+        request,
+        'contact/index.html', 
+        context
+    )     
     
