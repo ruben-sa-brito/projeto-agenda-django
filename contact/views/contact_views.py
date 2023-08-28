365c96389a20f02fa947e55b0b6921097aa0c635
Timewound <rubenjms1@gmail.com>
2023-08-28 10:17:23 -0300

    incluindo paginaçao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
+from django.core.paginator import Paginator
 
 def index(request):
     
     contacts = Contact.objects\
     .filter(show=True)\
-    .order_by('-id')[0:10]
+    .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'site_title':'Contatos - '
     }
     
@@ -55,8 +60,12 @@ def search(request):
             Q(email__icontains=search_value))\
     .order_by('-id')
     
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'site_title':'Search - '
     }
     
